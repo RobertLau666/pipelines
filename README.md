310c158b7b90d71799d10d166749db2c62700cfa
Robert Lau <1245725448@qq.com>
2025-12-04 16:24:24 +0800

    add LLM

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # MNIST_pipeline
+LLM 项目来源于 [RethinkFun b站](https://www.bilibili.com/video/BV1xhYtewEum?spm_id_from=333.788.videopod.sections&vd_source=f7b2ced6b1acdce7ce2389503354cfb8)，[代码地址](https://github.com/RethinkFun/LLM)
--- vision_transformer.py
+++ vision_transformer.py
@@ -1,7 +1,6 @@
 """
 blog: 
 https://blog.csdn.net/weixin_44791964/article/details/122637701?ops_request_misc=%257B%2522request%255Fid%2522%253A%25229ABA10D0-660F-4469-B688-C1AE22A1E811%2522%252C%2522scm%2522%253A%252220140713.130102334..%2522%257D&request_id=9ABA10D0-660F-4469-B688-C1AE22A1E811&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~all~top_positive~default-1-122637701-null-null.142^v100^pc_search_result_base3&utm_term=vit&spm=1018.2226.3001.4187
-
 """
 
 import math
